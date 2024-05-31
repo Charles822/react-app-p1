01d505686b52dddc4ad3365f079b65b55b1b3a50
Charles Fauchet <charles.fauchet@gmail.com>
2024-05-31 11:30:29 +0800

    Passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "Lyon", "La Rochelle", "Hong Kong"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
-  
-  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>
-
+  return (
+    <div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
