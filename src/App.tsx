fa9eebb33f9a5167da699823d4b4abecd49bad0f
Charles Fauchet <charles.fauchet@gmail.com>
2024-06-01 11:28:16 +0800

    Exercise 2 Alert and button finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <Button color='primary' onClick={() => console.log("Clicked")}>My Button</Button>
-    </div>
-  );
+  const [alertVisible, setAlertVisibility] = useState(false);
+    return (
+      <div>
+        {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>Holy</Alert>}
+        <Button color="primary" onClick={() => setAlertVisibility(true)}>
+          My Button
+        </Button>
+      </div>
+    );
 }
 
 export default App;
