b2e108a1d1c768773783998edc0c550e5da2e03c
Charles Fauchet <charles.fauchet@gmail.com>
2024-06-01 11:56:51 +0800

    CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import Button from "./components/Button";
-import Alert from "./components/Alert";
-import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const items = ["New York", "Los Angeles", "San Fransisco"]
     return (
       <div>
-        {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>Holy</Alert>}
-        <Button color="primary" onClick={() => setAlertVisibility(true)}>
-          My Button
-        </Button>
+        <ListGroup heading="Miami" items={items} onSelectItem={() => console.log(item)}></ListGroup>
       </div>
     );
 }
