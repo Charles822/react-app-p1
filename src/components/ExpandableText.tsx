3031246c4245a7aab520992d9e00b316f97f4280
Charles Fauchet <charles.fauchet@gmail.com>
2024-06-11 09:53:26 +0200

    Building an expandable text - Mosh solution

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -1,47 +1,66 @@
+import React from 'react'
 import { useState } from "react";
+
 
 interface Props {
 	children: string;
-	maxChars: number;
+	maxChars?: number; // ? to make it optionnal and add a default value
 	onClick: () => void;
 }
 
-function ExpandableText({ children, maxChars, onClick }: Props) {
-	const [text, setText] = useState(
-		{ children }["children"].substring(0, maxChars) + "...",
-	);
-	const [buttontText, setButtonText] = useState("More");
+const ExpandableText = ({children, maxChars = 100}: Props) => {
+	const [isExpanded, setExpanded] = useState(false);
 
-	return (
-		<>
-			<div>
-				{" "}
-				{text}
-				<button
-					onClick={() => {
-						if (buttontText === "More") {
-							setText({ children }["children"] + "...");
-							setButtonText("Less");
-						} else if (buttontText === "Less") {
-							setText(
-								{ children }["children"].substring(
-									0,
-									maxChars,
-								) + "...",
-							);
-							setButtonText("More");
-						}
-					}}
-				>
-					{buttontText}
-				</button>
-			</div>
-		</>
-	);
-}
+	if (children.length <= maxChars) return <p>{children}</p>;
 
-export default ExpandableText;
+	const text = isExpanded ? children : children.substring(0, maxChars);
 
-//setShorText(text.map(char => text += char)
-//{setShorText("abc")}
-//{setShorText(children.map(char => text[]
+	return <p>{text}...<button onClick={() => setExpanded(!isExpanded)}>{isExpanded ? 'Less' : 'More'}</button></p>;
+
+};
+
+export default ExpandableText
+
+
+// interface Props {
+// 	children: string;
+// 	maxChars: number;
+
+// 	onClick: () => void;
+// }
+
+// function ExpandableText({ children, maxChars, onClick }: Props) {
+// 	const [text, setText] = useState(
+// 		{ children }["children"].substring(0, maxChars) + "...",
+// 	);
+// 	const [buttontText, setButtonText] = useState("More");
+
+// 	return (
+// 		<>
+// 			<div>
+// 				{" "}
+// 				{text}
+// 				<button
+// 					onClick={() => {
+// 						if (buttontText === "More") {
+// 							setText({ children }["children"] + "...");
+// 							setButtonText("Less");
+// 						} else if (buttontText === "Less") {
+// 							setText(
+// 								{ children }["children"].substring(
+// 									0,
+// 									maxChars,
+// 								) + "...",
+// 							);
+// 							setButtonText("More");
+// 						}
+// 					}}
+// 				>
+// 					{buttontText}
+// 				</button>
+// 			</div>
+// 		</>
+// 	);
+// }
+
+// export default ExpandableText;
