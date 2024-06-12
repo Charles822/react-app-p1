593e51323d9321c2cb84f4b3b3d89791f7522803
Charles Fauchet <charles.fauchet@gmail.com>
2024-06-12 18:21:11 +0200

    Accesing input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-import {useState} from "react";
-import Button from "./components/Button";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {    
     return (
       <div>
-        <ExpandableText maxChars={20}>
-        Hello world
-        </ExpandableText>
+        <Form />
       </div>
     );
 }
 
 export default App;
-
-
-
-// function App() {
-//   const [cart, setCart] = useState({
-//     discount: .1,
-//     items: [
-//       { id: 1, title: 'Product 1', quantity: 1 },
-//       { id: 2, title: 'Product 2', quantity: 1 }
-//       ],
-
-//   });
-    
-//   const handleClick = () => {
-//     setCart({ ...cart, items: cart.items.map(item => item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item) })
-//   }
-//     return (
-//       <div>
-//         <Button onClick={handleClick}>Change name</Button>
-//         <p>{console.log({cart})}</p>
-//       </div>
-//     );
-// }
