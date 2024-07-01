c9ebafec8c086dacc6c099c636d7301bd3fa669e
Charles Fauchet <charles.fauchet@gmail.com>
2024-07-01 10:39:24 +0800

    Connect to baxckend, lesson 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import { useState } from "react";
-import ProductList from './components/ProductList'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
 
-  const [category, setCategory] = useState('');
+  useEffect(() => {
+
+    const res = await 
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => console.log(setUsers(res.data)))
+      .catch(err => console.log(setError(err.message)));
+  }, [])
 
   return (
-    <div>
-      <select className="form-select">
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="HouseHold">HouseHold</option>
-      </select>
-      <ProductList />
-    </div>
+    <>
+    {error && <p className="text-danger">{error}</p>}
+    <ul>{users.map(user => <li key={user.id}>{user.name}</li>)}</ul>
+    </>
   );
 }
 
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 
-const ProductList = () => {
+const ProductList = ({ category } : { category: string }) => {
 	const [products, setProducts] = useState<string[]>([]);
 
 	useEffect(() => {
-		console.log("Fetching products");
+		console.log("Fetching products in", category);
 		setProducts(['Clothing', 'Household']);
-	}, []);
+	}, [category]); // the second argument refers to dependencies, if no arguments: useEffect is called every time there is a new render. empty array equal to executing once only. 
 
 	return <div>ProductList</div>;
 };
